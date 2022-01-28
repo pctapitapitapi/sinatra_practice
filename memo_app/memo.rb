c3c6cbe539409c23fcbf6268fd4d7a05b0a7b913
pctapitapitapi <emi.yamamoto73@gmail.com>
2022-01-28 15:51:00 +0900

    jsonファイルを配列データで初期化修正

--- memo_app/memo.rb
+++ memo_app/memo.rb
@@ -4,14 +4,15 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'erb'
 require 'json'
+require 'cgi'
 
 memos = nil
 
 def open_file
-  if File.read('memo.json', 1).nil?
-    nil
-  else
+  begin
     JSON.parse(File.read('memo.json'), symbolize_names: true)
+  rescue JSON::ParserError => e
+    puts "ファイルが空です。ERROR: #{e}"
   end
 end
 
@@ -27,7 +28,6 @@ end
 
 post '/memos' do
   memos = open_file
-  memos = [] if memos.nil?
   new_memo = {
     id: SecureRandom.uuid,
     title: params[:title],
@@ -41,8 +41,7 @@ end
 
 get '/memos/:id' do
   memos = open_file
-  if memos.find { |m| m[:id] == params[:id] }
-    @memo = memos.find { |m| m[:id] == params[:id] }
+  if (@memo = memos.find { |m| m[:id] == params[:id] })
     erb :show
   else
     erb :not_found
@@ -51,16 +50,22 @@ end
 
 get '/memos/:id/edit' do
   memos = open_file
-  @memo = memos.find { |m| m[:id] == params[:id] }
-  erb :edit
+  if (@memo = memos.find { |m| m[:id] == params[:id] })
+    erb :edit
+  else
+    erb :not_found
+  end
 end
 
 patch '/memos/:id' do
   memos = open_file
-  memo = memos.find { |m| m[:id] == params[:id] }
-  memo[:title] = params[:title]
-  memo[:content] = params[:content]
-  memo[:create_at] = Time.now
+  if (memo = memos.find { |m| m[:id] == params[:id] })
+    memo[:title] = params[:title]
+    memo[:content] = params[:content]
+  else
+    erb :not_found
+    break
+  end
   File.open('memo.json', 'w') { |file| JSON.dump(memos, file) }
   redirect '/memos'
 end
@@ -70,12 +75,6 @@ delete '/memos/:id' do
   memos.delete_if { |n| n[:id] == params[:id] }
   File.open('memo.json', 'w') { |file| JSON.dump(memos, file) }
   redirect '/memos'
-end
-
-helpers do
-  def escape_html(text)
-    Rack::Utils.escape_html(text)
-  end
 end
 
 not_found do
